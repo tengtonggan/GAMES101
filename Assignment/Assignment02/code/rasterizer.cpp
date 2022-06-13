fa83ead4894359c882adc470929290955b11a865
ALei <812537756@qq.com>
2022-06-13 16:10:59 +0800

    -w-->w in assignment02

--- Assignment/Assignment02/code/rasterizer.cpp
+++ Assignment/Assignment02/code/rasterizer.cpp
@@ -140,7 +140,7 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t, Antialiasing anti) {
                     auto [alpha, beta, gamma] = computeBarycentric2D(x + 0.5, y + 0.5, t.v);
                     float w_reciprocal = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
                     float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
-                    z_interpolated *= -w_reciprocal;
+                    z_interpolated *= w_reciprocal;
                     float& z_buffer = depth_buf[(height - 1 - y) * width + x];
                     if (z_interpolated < depth_buf[(height - 1 - y) * width + x]) {
                         z_buffer = z_interpolated;
